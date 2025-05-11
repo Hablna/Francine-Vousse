8ae225d134d0cc9b7cbda76f65119700106f03a8
Hablna <habibhalirou@gmail.com>
2025-05-11 13:49:24 +0200

    #29 #33 Mise en place de la template et les endpoints utiles

--- src/Service/ApiService.php
+++ src/Service/ApiService.php
@@ -11,9 +11,40 @@ class ApiService
         $this->client = $client;
     }
 
-    public function getPlanifications(){
+    public function getAllSpectacles(){
         $reponses = $this->client->request('GET', 'http://172.16.124.35/api/Spectacles/GetAllSpectacles');
+         if ($reponses->getStatusCode() === 200){
+             return $reponses->toArray();
+         }
+         return null;
+    }
+    public function getPlanifications($id){
+        $reponses = $this->client->request('GET', 'http://172.16.124.35/api/Spectacles/GetPlanifications/',[
+            'query'=>[
+                'id'=>$id
+            ],
+        ]);
+        if ($reponses->getStatusCode() === 200){
+            return $reponses->toArray();
+        }
+        return null;
+    }
 
-        return $reponses->toArray();
+    public function checkBillet(int $idBillet, int $idSpectacle): bool
+    {
+        $response = $this->client->request('POST', 'http://adresse-api/api/Spectacles/CheckBillet', [
+            'json' => [
+                'idBillet' => $idBillet,
+                'idSpectacle' => $idSpectacle,
+            ],
+        ]);
+
+        // Vérifie que la requête a bien fonctionné
+        if ($response->getStatusCode() !== 200) {
+            throw new \Exception("Erreur lors de la vérification du billet.");
+        }
+
+        // Puisque l’API retourne un booléen pur, utilise `getContent()` et cast en bool
+        return filter_var($response->getContent(), FILTER_VALIDATE_BOOLEAN);
     }
 }
