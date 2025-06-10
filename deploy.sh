0499174e826a63d6a73abd1d16b5e518ef9ab294
Hablna <habibhalirou@gmail.com>
2025-06-10 12:10:01 +0200

    #31 #36 Gestion des pages d'atterrissage et la barre de recherche sur les spectacles.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "### Synchronisation des fichiers RSYNC ###"
-rsync  -e 'ssh' -S -av ./ habib@172.16.124.15:/var/www/Vousse  --include="public/.htaccess" --include=".env"  --include=".env.prod.local" --include="public/build/manifest.json" --exclude-from=".gitignore" --exclude=".*"
+rsync  -e 'ssh' -S -av ./ habib@172.16.124.15:/var/www/Vousse --include="public/.htaccess" --include=".env"  --include=".env.prod.local" --include="public/build/manifest.json" --exclude-from=".gitignore" --exclude=".*"
 echo "### CONNECTION SSH ###"
 ssh habib@172.16.124.15 -o "StrictHostKeyChecking=no" <<'eof'
 echo "### Déplacement dans le dossier web ###"
@@ -11,5 +11,5 @@ echo "### COMPOSER INSTALL ###"
 composer install
 
 echo "### DOCTRINE MIGRATION ###"
-symfony console --no-interaction doctrine:database:create --if-not-exists
+symfony console doctrine:database:create --no-interaction || true
 symfony console --no-interaction doctrine:migrations:migrate
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -37,7 +37,7 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devez acceptez nos termes.',
                     ]),
                 ],
             ])
@@ -48,11 +48,11 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez renseigner un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
